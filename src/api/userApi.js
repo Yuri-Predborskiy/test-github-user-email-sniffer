1c9c71231b07536d2cd6ba17ccfd9a5b39ded33e
Yuri Predborskiy <shini1984@gmail.com>
2018-07-23 09:17:35 +0300

    Remove setup route - redundant, there is now a registration route.

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -12,22 +12,7 @@ function getCurrentUser(req, res) {
     });
 }
 
-function setup(req, res) {
-    let yuri = new User({
-        name: 'Yuri Predborski',
-        password: 'safePassword!'
-    });
-
-    // save the sample user
-    yuri.save(function(err) {
-        if (err) { throw err; }
-        console.log('User saved successfully');
-        res.json({ success: true });
-    });
-}
-
 module.exports = {
     getAll,
     getCurrentUser,
-    setup,
 };
