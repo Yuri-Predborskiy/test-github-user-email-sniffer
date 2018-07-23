2e18c3c3173167a01d51b4c7ecb027bf0ae91ed5
Yuri Predborskiy <shini1984@gmail.com>
2018-07-24 00:52:02 +0300

    Refactor github api controller into promises and async functions.

--- src/api/github.js
+++ src/api/github.js
@@ -2,81 +2,109 @@
 const https = require('https');
 const config = require('../../config/config');
 
-function sendMailToGitHubUsers(req, res) {
+async function sendMailToGitHubUsers(req, res) {
     console.log('starting mail sender');
     let users = req.body.github_users.replace(/\s/g, '').split(',');
-    let results = [];
+    let promises = users.map(getUserData);
+    users = await Promise.all(promises);
+    promises = users.map(getWeather);
+    users = await Promise.all(promises);
+    // promises = users.map(sendMail);
+    // let result = await Promise.all(promises);
+    res.json({ success: true, data: users });
+}
 
-    let promises = users.map(function(name) {
-        let options = {
-            headers: {
-                'User-Agent': config.userAgent,
-                'Authorization': `token ${config.accessToken}`,
-            },
-            host : `api.github.com`,
-            path: `/users/${name}`
-        };
-        return new Promise(function(resolve, reject) {
-            https.get(options, (resp) => {
-                let data = '';
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-                resp.on('end', () => {
-                    // results.push(JSON.parse(data));
-                    let dp = JSON.parse(data);
-                    let res = {
-                        name: dp.name,
-                        email: dp.email,
-                        location: dp.location ? dp.location.split(',')[0].trim() : null,
-                    };
-                    if (res.location) {
-                        let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${res.location}&APPID=${config.weatherAPIkey}`;
-                        https.get(weatherURL, (resp) => {
-                            let data = '';
-                            resp.on('data', (chunk) => {
-                                data += chunk;
-                            });
-                            resp.on('end', () => {
-                                data = JSON.parse(data);
-                                if (data.cod === 200) {
-                                    res.weather = data.main;
-                                }
-                                results.push(res);
-                                resolve();
-                            });
-                        }).on("error", (err) => {
-                            console.log("Error: " + err.message);
-                            reject();
-                        });
-                    } else {
-                        results.push(res);
-                        resolve();
-                    }
-                });
-            }).on("error", (err) => {
-                console.log("Error: " + err.message);
-                reject();
+function getUserData(username) {
+    let options = {
+        headers: {
+            'User-Agent': config.userAgent,
+            'Authorization': `token ${config.accessToken}`,
+        },
+        host : `api.github.com`,
+        path: `/users/${username}`
+    };
+    return new Promise(function(resolve, reject) {
+        https.get(options, (resp) => {
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
             });
+            resp.on('end', () => {
+                let dp = JSON.parse(data);
+                let res = {
+                    login: dp.login,
+                    name: dp.name,
+                    email: dp.email,
+                    location: dp.location ? dp.location.split(',')[0].trim() : null,
+                };
+                resolve(res);
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            reject(err);
         });
     });
+}
 
-    Promise.all(promises)
-        .then(() => {
-            res.json({ success: true, message: 'User data collected', data: results });
-        })
-        .catch(err => {
-            console.error(err);
-            res.json({ success: false, message: err });
+function getWeather(user) {
+    let location = user.location ? user.location.split(',')[0].trim() : null;
+    if (!location) {
+        return new Promise((resolve) => resolve(user));
+    }
+    let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${config.weatherAPIkey}&units=metric`;
+    return new Promise(function(resolve, reject) {
+        https.get(weatherURL, (resp) => {
+            let data = '';
+            resp.on('data', (chunk) => {
+                data += chunk;
+            });
+            resp.on('end', () => {
+                data = JSON.parse(data);
+                if (data.cod === 200) {
+                    user.weather = data.main;
+                }
+                resolve(user);
+            });
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+            resolve(user);
         });
+    });
+}
 
+function sendMail(user) {
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: account.user, // generated ethereal user
+            pass: account.pass // generated ethereal password
+        }
+    });
 
-    // 1 - get list of desired users from query
-    // 2 - get message text
-    // 3 - if any users contain email - send mail
-    // return success if everything went well
-    // return success false if couldn't get github access
-    // send report for each user
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: 'bar@example.com, baz@example.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+
 }
 
 module.exports = {
